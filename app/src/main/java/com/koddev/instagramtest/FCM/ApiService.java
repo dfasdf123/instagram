e30b10f3b20f2c42b8c42b18b5dd8652dda36b98
dfasdf123 <133076756+dfasdf123@users.noreply.github.com>
2023-06-30 17:28:16 +0900

    Update ApiService.java

--- app/src/main/java/com/koddev/instagramtest/FCM/ApiService.java
+++ app/src/main/java/com/koddev/instagramtest/FCM/ApiService.java
@@ -8,7 +8,7 @@ import retrofit2.http.POST;
 public interface ApiService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAAqxRc0Cw:APA91bF-OcesLqDX9vYAyzEckUCpq2WUs3bk7pi76obOO4Jmk2k_wxHbL5cKn4ydX0vl34iPjiHrfcKhFhT21YQP-57JT2r4L9ZqP1BlkR5JoGHmNmPzMd8gABLJ3r5KQk_Mv6-kaJj2"  // FCM 서버 키
+            ""  // FCM 서버 키
     })
     @POST("fcm/send")
     Call<MyResponse> sendNotification(@Body Sender body);
